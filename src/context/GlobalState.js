07392baf011801b09e6a844054c1680f61b6e7b4
Mike Conrad <mike@enxo.co>
2021-09-11 09:32:53 -0400

    Initial commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,41 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+const initialState = {};
+// Initial State
+if (JSON.parse(localStorage.getItem("transactions"))) {
+  initialState.transactions = JSON.parse(localStorage.getItem("transactions"));
+} else {
+  initialState.transactions = [];
+}
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
